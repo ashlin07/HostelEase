a7cf13eb746a832e22c9b25deb588a2d5d34888c
Anshul Ranjan <ItsAnshulRanjan@gmail.com>
2024-04-13 01:05:20 +0530

    added mess

--- src/main/java/com/management/hostelease/model/Mess.java
+++ src/main/java/com/management/hostelease/model/Mess.java
@@ -1,6 +1,14 @@
 package com.management.hostelease.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Mess {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String messName;
     private String messType;
