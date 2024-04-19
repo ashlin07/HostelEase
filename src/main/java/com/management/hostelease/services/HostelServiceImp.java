2101af28e9e7335fa50522747c9222fd6679a16e
ashlin07 <furtadoashlin07@gmail.com>
2024-04-19 12:05:11 +0530

    removed views

---
+++ src/main/java/com/management/hostelease/services/HostelServiceImp.java
@@ -0,0 +1,14 @@
+package com.management.hostelease.services;
+
+import com.management.hostelease.model.Block;
+import com.management.hostelease.model.Hostel;
+
+public class HostelServiceImp {
+    public void addBlock(Hostel hostel, Block block) {
+        // Implementation goes here
+    }
+
+    public void removeBlock(Hostel hostel, Block block) {
+        // Implementation goes here
+    }
+}
--- src/main/java/com/management/hostelease/services/RoomServiceImp.java
+++ src/main/java/com/management/hostelease/services/RoomServiceImp.java
@@ -8,15 +8,18 @@ public class RoomServiceImp implements RoomService {
     @Override
     public void addStudent(Room room, String student) {
         // Implementation goes here
+
     }
 
     @Override
     public void removeStudent(Room room, String student) {
         // Implementation goes here
+
     }
 
     @Override
     public void bookRoom(Room room) {
         // Implementation goes here
+
     }
 }
